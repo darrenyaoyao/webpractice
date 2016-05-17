4fcb89fd480a4bc0cbca13b8ef92c16e55d91c16
darrenyaoyao <b02901042@ntu.edu.tw>
2016-05-17 14:38:25 +0800

    Prototype

--- midterm-hackthon/server/api.js
+++ midterm-hackthon/server/api.js
@@ -2,14 +2,14 @@ const Router = require('express').Router;
 const router = new Router();
 
 const data = [{
-	"id" : 01,
+	"id" : 1,
 	"img" : "img/1.jpg",
 	"question" : "這是什麼水果?",
 	"choices" : ["香蕉", "蘋果", "芭樂", "西瓜", "榴槤"],
 	"answer" : 0
 },
 {
-	"id" : 02,
+	"id" : 2,
 	"img" : "img/2.jpg",
 	"question" : "這顆蘋果是什麼顏色?",
 	"choices" : ["紅色", "白色", "綠色", "藍色", "紫色"],
